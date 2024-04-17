fcb803ff4badf9f6141ac81ee79865f0f4b3fb59
Thiago Sawada <tms1_tms2@hotmail.com>
2024-04-17 18:22:57 -0300

    feat: Director class

---
+++ src/creational/builder/classes/director.ts
@@ -0,0 +1,14 @@
+import { MealBuilderProtocol } from "../interfaces/meal-builder-protocol";
+
+export class Director {
+  private _builder: MealBuilderProtocol | null = null;
+
+  setMealBuilder(builder: MealBuilderProtocol): void {
+    this._builder = builder;
+  }
+
+  buildMainDish(): void {
+    this._builder?.makeMeal();
+    this._builder?.makeBeverage();
+  }
+}
--- src/creational/builder/main.ts
+++ src/creational/builder/main.ts
@@ -1,16 +1,21 @@
+import { Director } from './classes/director';
 import { MainDishBuilder } from './classes/main-dish-builder';
 import { VeganDishBuilder } from './classes/vegan-dish-builder';
 
-const mainDishBuilder = new MainDishBuilder();
-mainDishBuilder.makeMeal();
-console.log(mainDishBuilder.getMeal());
-console.log(mainDishBuilder.getPrice());
+function clientCode(director: Director) {
+  const mainDishBuilder = new MainDishBuilder();
+  director.setMealBuilder(mainDishBuilder);
+  director.buildMainDish();
+  mainDishBuilder.makeDessert();
+  console.log(mainDishBuilder.getMeal());
+  console.log(mainDishBuilder.getPrice());
 
-mainDishBuilder.reset();
-const meal2 = mainDishBuilder.makeBeverage().getMeal();
-console.log(meal2);
+  const veganBuilder = new VeganDishBuilder();
+  director.setMealBuilder(veganBuilder);
+  director.buildMainDish();
+  console.log(veganBuilder.getMeal());
+  console.log(veganBuilder.getPrice());
+}
 
-const veganDishBuilder = new VeganDishBuilder();
-const veganMeal = veganDishBuilder.makeMeal().getMeal();
-console.log(veganMeal);
-console.log(veganMeal.getPrice());
+const director = new Director();
+clientCode(director);
