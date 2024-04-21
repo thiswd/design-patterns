978222ac85c45245c983143b5d1b5031df235193
Thiago Sawada <tms1_tms2@hotmail.com>
2024-04-21 16:00:11 -0300

    feat: add decorator example

--- src/structural/decorator/main.ts
+++ src/structural/decorator/main.ts
@@ -4,8 +4,13 @@ import { ProductCustomizationDecorator } from './product/product-customization-d
 
 const tShirt = new TShirt();
 const tShirtWithStamp = new ProductStampDecorator(tShirt);
+const tShirtStampFrontAndBack = new ProductStampDecorator(tShirtWithStamp);
 const customizedTShirt = new ProductCustomizationDecorator(tShirt);
 
 console.log(tShirt.getPrice(), tShirt.getName());
 console.log(tShirtWithStamp.getPrice(), tShirtWithStamp.getName());
+console.log(
+  tShirtStampFrontAndBack.getPrice(),
+  tShirtStampFrontAndBack.getName(),
+);
 console.log(customizedTShirt.getPrice(), customizedTShirt.getName());
