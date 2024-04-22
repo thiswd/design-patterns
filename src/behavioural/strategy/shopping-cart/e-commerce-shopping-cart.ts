4602b1361ae5b5403e81ec37ebbe1c5edb8be833
Thiago Sawada <tms1_tms2@hotmail.com>
2024-04-22 18:12:35 -0300

    refactor: rename class attribute

--- src/behavioural/strategy/shopping-cart/e-commerce-shopping-cart.ts
+++ src/behavioural/strategy/shopping-cart/e-commerce-shopping-cart.ts
@@ -2,19 +2,19 @@ import { ECommerceProductProtocol } from './e-commerce-product-protocol';
 import { DiscountStrategy } from './discount-strategy';
 
 export class ECommerceShoppingCart {
-  private products: ECommerceProductProtocol[] = [];
+  private _products: ECommerceProductProtocol[] = [];
   private _discountStrategy: DiscountStrategy = new DiscountStrategy();
 
   addProduct(...products: ECommerceProductProtocol[]): void {
-    products.forEach((product) => this.products.push(product));
+    products.forEach((product) => this._products.push(product));
   }
 
   getProducts(): ECommerceProductProtocol[] {
-    return this.products;
+    return this._products;
   }
 
   getTotal(): number {
-    return this.products.reduce((sum, product) => sum + product.price, 0);
+    return this._products.reduce((sum, product) => sum + product.price, 0);
   }
 
   getTotalWithDiscount(): number {
