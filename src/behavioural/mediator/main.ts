85b2e62080370a018d23ffe90673e70774f64400
Thiago Sawada <tms1_tms2@hotmail.com>
2024-04-24 17:40:44 -0300

    refactor: add products

--- src/behavioural/mediator/main.ts
+++ src/behavioural/mediator/main.ts
@@ -5,12 +5,17 @@ import { Buyer } from './buyer';
 const mediator = new Mediator();
 
 const seller1 = new Seller();
-seller1.addProduct({ id: '1', name: 'Camiseta', price: 49.9 });
-seller1.addProduct({ id: '2', name: 'Caneta', price: 9.9 });
+seller1.addProduct(
+  { id: '1', name: 'Camiseta', price: 49.9 },
+  { id: '2', name: 'Caneta', price: 9.9 },
+);
 
 const seller2 = new Seller();
-seller2.addProduct({ id: '3', name: 'Carro', price: 49000.9 });
-seller2.addProduct({ id: '4', name: 'Lápis', price: 1.9 });
+seller2.addProduct(
+  { id: '3', name: 'Carro', price: 49000.9 },
+  { id: '4', name: 'Lápis', price: 1.9 },
+  { id: '5', name: 'Caneca', price: 10.9 },
+);
 
 mediator.addSeller(seller1, seller2);
 
