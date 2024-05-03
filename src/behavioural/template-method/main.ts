78ab745cb7ddead6bdf74adfba2151e599e1bd62
Thiago Sawada <thiago.sawada@compasso.com.br>
2024-05-03 17:59:05 -0300

    change file paths

--- src/behavioural/template-method/main.ts
+++ src/behavioural/template-method/main.ts
@@ -3,14 +3,14 @@ import { CustomerDataParserTxt } from './customer-data-parser-txt';
 import { CustomerDataParserJson } from './customer-data-parser-json';
 
 async function run() {
-  const filePathTxt = resolve(__dirname, 'files', 'customer.txt');
+  const filePathTxt = resolve(__dirname, 'files', 'customers.txt');
   const customerDataParserTxt = new CustomerDataParserTxt(filePathTxt);
   await customerDataParserTxt.fixCustomerData();
   console.log(customerDataParserTxt.customerData);
 
   console.log();
 
-  const filePathJson = resolve(__dirname, 'files', 'customer.json');
+  const filePathJson = resolve(__dirname, 'files', 'customers.json');
   const customerDataParserJson = new CustomerDataParserJson(filePathJson);
   await customerDataParserJson.fixCustomerData();
   console.log(customerDataParserJson.customerData);
